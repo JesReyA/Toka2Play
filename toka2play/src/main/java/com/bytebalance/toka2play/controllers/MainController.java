029c7ac10f694c5c7af85c2e71e5dd2e633be9b1
JesReyA <jesreyav@gmail.com>
2026-04-07 17:39:18 -0600

    cambios en nombres de imagenes

--- toka2play/src/main/java/com/bytebalance/toka2play/controllers/MainController.java
+++ toka2play/src/main/java/com/bytebalance/toka2play/controllers/MainController.java
@@ -6,5 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/MainApp/index.html";
+    }
 }
