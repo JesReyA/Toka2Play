6cbda0a84db8951b5b21351cdf0bab691fb6b545
JesReyA <jesreyav@gmail.com>
2026-04-07 12:54:21 -0600

    commit

---
+++ toka2play/src/main/java/com/bytebalance/toka2play/Toka2playApplication.java
@@ -0,0 +1,13 @@
+package com.bytebalance.toka2play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toka2playApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toka2playApplication.class, args);
+	}
+
+}
---
+++ toka2play/src/test/java/com/bytebalance/toka2play/Toka2playApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytebalance.toka2play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toka2playApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
