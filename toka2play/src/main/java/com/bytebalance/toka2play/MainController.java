aae0ae3b1c53e17666e6ad0791ca50df90ce1979
erikazr <erikazael47@gmail.com>
2026-04-07 23:44:16 -0600

    templates mapeados ya equisde equisde

--- toka2play/src/main/java/com/bytebalance/toka2play/MainController.java
+++ toka2play/src/main/java/com/bytebalance/toka2play/MainController.java
@@ -5,31 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-
     @GetMapping({"/", "/index"})
     public String index() {
-<<<<<<< HEAD:toka2play/src/main/java/com/bytebalance/toka2play/controllers/MainController.java
-        return "redirect:/index.html";
-=======
-        // Esto busca el archivo "index.html" dentro de la carpeta templates
         return "index";
->>>>>>> 32efe7030e577409ca8b4c2030b07f762f7c0bce:toka2play/src/main/java/com/bytebalance/toka2play/MainController.java
-    }
-    @GetMapping("/arkanoid")
-    public String arkanoid() {
-<<<<<<< HEAD:toka2play/src/main/java/com/bytebalance/toka2play/controllers/MainController.java
-        return "redirect:/arkanoid.html";
-=======
-        return "arkanoid";
->>>>>>> 32efe7030e577409ca8b4c2030b07f762f7c0bce:toka2play/src/main/java/com/bytebalance/toka2play/MainController.java
     }
 
-    @GetMapping("/snake")
-    public String snake() {
-<<<<<<< HEAD:toka2play/src/main/java/com/bytebalance/toka2play/controllers/MainController.java
-        return "redirect:/snake.html";
-=======
-        return "snake";
->>>>>>> 32efe7030e577409ca8b4c2030b07f762f7c0bce:toka2play/src/main/java/com/bytebalance/toka2play/MainController.java
-    }
+    @GetMapping("/minijuegos/snake")
+    public String snake() { return "snake"; }
+
+    @GetMapping("/minijuegos/arkanoid")
+    public String arkanoid() { return "arkanoid"; }
+
+    @GetMapping("/minijuegos/doodle")
+    public String doodle() { return "paginalnicioMiniApp"; }
 }
