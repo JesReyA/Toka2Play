79590a4bbb646d163cb7aaa523661ebfb21215b6
Malvapq <todorandom21@gmail.com>
2026-04-08 18:04:00 -0600

    arreglo key groq

--- toka2play/src/main/java/com/bytebalance/toka2play/Toka2playApplication.java
+++ toka2play/src/main/java/com/bytebalance/toka2play/Toka2playApplication.java
@@ -1,5 +1,6 @@
 package com.bytebalance.toka2play;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Toka2playApplication {
 
 	public static void main(String[] args) {
+		// Carga el archivo .env
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		// Inyecta las variables en el sistema de propiedades de Java
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(Toka2playApplication.class, args);
 	}
-
 }
